082ecfa18689b00ac19458d3b3681d1f45a47641
Billy O'Mahony <omahonybilly@gmail.com>
2019-08-08 14:15:56 +0100

    Declaring variables/ types of variables in R

---
+++ Section 2/Section2.R
@@ -0,0 +1,34 @@
+
+
+# Integer
+# Capital L after number makes it an integer
+x <- 2L 
+
+# Check the datatype of x
+typeof(x)
+
+
+# Double
+y <- 2.5
+typeof(y)
+
+
+# Complex Number
+z <- 3 + 2i
+typeof(z)
+
+
+# Character
+a = "c"
+typeof(a)
+
+b = "Char"
+typeof(b)
+
+
+# Logical (Boolean)
+q <- TRUE
+typeof(q)
+
+r <- F
+typeof(q)
