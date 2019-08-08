87809869be02c1ec9873e6c11d25ccbd8e083e6a
Billy O'Mahony <omahonybilly@gmail.com>
2019-08-08 14:27:36 +0100

    Basic use of variables

---
+++ Section 2/Section2Lec10.R
@@ -0,0 +1,28 @@
+# Arithimatic 
+A <- 10
+B <- 5
+C = A + B
+# To see the value of a variable simply type the variable
+C 
+
+# Division
+var1 <- 2.5
+var2 <- 4
+result <- var1 / var2 
+result
+
+# Using Functions
+answer = sqrt(result)
+answer
+
+# Working with Characters
+greeting <- "Hello"
+name <- "John"
+# Concatenation in R
+message <- paste(greeting, name) 
+
+
+# Testing with brackets etc. 
+var3 <- 7.5
+result1 <- (var1 + var2) / var3 - B
+result1
