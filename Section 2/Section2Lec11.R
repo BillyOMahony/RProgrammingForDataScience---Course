6931a1f6d3be3297af11709462e6e9a84e2e1c4e
Billy O'Mahony <omahonybilly@gmail.com>
2019-08-08 14:35:42 +0100

    Use of Logical operators

---
+++ Section 2/Section2Lec11.R
@@ -0,0 +1,42 @@
+# Logical Operators can be either 
+# TRUE (T) 
+# FALSE (F)
+
+# The result of a logical statement will print out
+4 < 5
+
+# The result of a logical statement can also be assigned to a variable
+Res1 <- 4 < 5
+Res1
+
+Res2 <- 10 > 100
+Res2
+
+Res3 <- 3 == 3
+Res3
+
+Res4 <- 4 != 4
+Res4
+
+Res5 <- !Res4
+Res5
+
+Res6 <- Res5 & Res4
+Res6
+
+Res7 <- Res4 | Res5
+Res7
+
+# All logical operators
+# ==
+# !=
+# <
+# >
+# <=
+# >=
+# !
+# | (OR)
+# & (AND)
+# isTrue(x)
+
+
