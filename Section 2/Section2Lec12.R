b55033424f67342ed277e2f65a577d9be5a1d5a0
Billy O'Mahony <omahonybilly@gmail.com>
2019-08-08 15:01:34 +0100

    R While loops

---
+++ Section 2/Section2Lec12.R
@@ -0,0 +1,20 @@
+
+
+# This while loop will not run
+while(F){
+  print("Hello")
+}
+
+# This while loop will run forever
+while(T){
+  print("Hello")
+}
+
+
+# A loop which runs a set ammount of times
+counter <- 0
+
+while(counter < 12){
+  counter <- counter + 1
+  print(counter)
+}
