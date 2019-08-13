8f773ce3010d8c7693cea221288c8966d4784d54
Billy O'Mahony <omahonybilly@gmail.com>
2019-08-13 15:29:41 +0100

    Basic intro to R functions

---
+++ Section 3/Sec3Lec24_Functions.R
@@ -0,0 +1,16 @@
+
+# Typing in a questionmark before a function brings up "help" on that funciton, arguements etc. 
+?rnorm()
+
+# Parameters can be specified as entered, therefore they don't have to be passed in sequence
+rnorm(n=5, mean=10, sd=20)
+
+
+?c()
+# Other paramerters can be found with the ? 
+?seq()
+seq(from=10, to=15, length.out = 2)
+
+?rep()
+
+?sqrt()
