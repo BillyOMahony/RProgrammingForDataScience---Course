2e3edda85c8d27bf1ce7629442f0d6095ea7affe
Billy O'Mahony <omahonybilly@gmail.com>
2019-08-14 12:02:17 +0100

    Creating a function

---
+++ Section 4/Sec4Lec38_CreatingAFunction.R
@@ -0,0 +1,13 @@
+
+
+# Create a function with parameters
+# Function is names myplot
+# rows parameter has a default value of 1:10
+myplot <- function(data, rows=1:10){
+  Data <- data[rows,,drop=F]
+  matplot(t(Data), type="b", pch=16, col=c(1:3)) #type = b plots lines between each point
+  legend("bottomleft", inset=0.01, legend=Players[rows], pch=16, col=c(1:3), horiz=F)
+}
+
+# Call the function we just made
+myplot(Points ,1:5)
