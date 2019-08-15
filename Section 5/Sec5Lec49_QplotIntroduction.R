17f75388bd047ab2b0ad8f00a0c712853396b8b9
Billy O'Mahony <omahonybilly@gmail.com>
2019-08-15 12:24:05 +0100

    Intro to Qplot

---
+++ Section 5/Sec5Lec49_QplotIntroduction.R
@@ -0,0 +1,20 @@
+
+
+# Import Data
+stats <- read.csv("RProgrammingForDataScience - Course\\Section 5\\P2-Demographic-Data.csv")
+head(stats)
+
+?qplot() # Can't see any information on qplot
+
+# We need to use the package with qplot
+library(ggplot2)
+?qplot() # We can now use qplot
+
+# Plots Internet users data as a histogram
+qplot(data=stats, x=Internet.users)
+
+# plots Income group against Birth rate, note that qplot automatically selected the right plot type (scatter plot here)
+qplot(data=stats, x=Income.Group, y=Birth.rate)
+
+# Changes the size of the plot
+qplot(data=stats, x=Income.Group, y=Birth.rate, size=I(2), colour=I("red"))
