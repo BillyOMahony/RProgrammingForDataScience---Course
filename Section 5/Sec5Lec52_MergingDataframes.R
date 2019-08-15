a215880eef0f12cc0ea6dd304440b22d594e729c
Billy O'Mahony <omahonybilly@gmail.com>
2019-08-15 14:35:45 +0100

    Section 5 Challenge Attempt

--- Section 5/Sec5Lec52_MergingDataframes.R
+++ Section 5/Sec5Lec52_MergingDataframes.R
@@ -13,3 +13,27 @@ head(mergedstats)
 # We end up at two Country columns in the merged table, so we simply remove one
 mergedstats$Country <- NULL
 str(mergedstats)
+
+
+
+# ---------- LECTURE 53: VISUALIZING WITH QPLOT PART II ----------
+
+library(ggplot2)
+
+# Plot Birth rate against Internet users. Colour each country based on it's region
+qplot(data=mergedstats, x=Birth.rate, y=Internet.users, colour=Region, size=I(3))
+
+# Graphs can also have different types of points other than circles
+qplot(data=mergedstats, x=Birth.rate, y=Internet.users, colour=Region, size=I(2), shape=I(15))
+
+# Different shapes can be assigned to different categories, similar to colours
+qplot(data=mergedstats, x=Birth.rate, y=Internet.users, colour=Region, size=I(2), shape=Income.Group)
+
+# Points on the graph can have different transparancy
+qplot(data=mergedstats, x=Birth.rate, y=Internet.users, colour=Region, size=I(5), alpha=I(0.5))
+
+# Titles can also be added
+qplot(data=mergedstats, x=Birth.rate, y=Internet.users, colour=Region, size=I(5), alpha=I(0.5), main="Birth Rate vs Internet Users")
+
+
+      
